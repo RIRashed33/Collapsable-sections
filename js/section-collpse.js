b576ef21dc46a0c8d990e994e36684b32b8ae7f5
RIRashed33 <rashedul@seopage1.net>
2025-02-27 17:07:46 +0600

    2nd section changed

--- js/section-collpse.js
+++ js/section-collpse.js
@@ -4,21 +4,33 @@ document.querySelectorAll(".single-collapsable-item .collapse-toggle button").fo
     });
 });
 
-document.querySelectorAll("button.collapsable-table-toggle").forEach(button => {
+document.querySelectorAll(".table-collapse-container .collasable-images, .table-collapse-container .collapsable-content").forEach(button => {
     button.addEventListener("click", function () {
-        this.closest(".collapsable-content").classList.toggle("active");
+        this.closest(".table-collapse-container").classList.toggle("active");
     });
 });
+
+
+document.querySelectorAll('.collasable-images, .collapsable-content').forEach(content => {
+    content.addEventListener('mouseenter', () => {
+        content.closest(".table-collapse-container").classList.add("hover");
+    });
+
+    content.addEventListener('mouseleave', () => {
+        content.closest(".table-collapse-container").classList.remove("hover");
+    });
+});
+
 
 // (function($){
 //     $(document).ready(function () {
 
 //         $("button.collapsable-table-toggle").click(function () {
-//             let parent = $(this).closest(".collapsable-content");
+//             let parent = $(this).closest(".table-collapse-container");
             
 //             parent.toggleClass("active");
             
-//             parent.find(".collapsable-table-content").stop(true, true).toggleClass("open").animate({
+//             parent.find(".right-collapsable-content").stop(true, true).toggleClass("open").animate({
 //                 maxWidth: "toggle"
 //             }, 3000);
 //         });
